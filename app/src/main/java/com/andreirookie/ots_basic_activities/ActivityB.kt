d25ae52117201fd55a3570322b34a86fd5d3dbd8
AndreiRookie <andrey.idem@gmail.com>
2023-02-09 23:58:44 +0400

    task2 done

--- app/src/main/java/com/andreirookie/ots_basic_activities/ActivityB.kt
+++ app/src/main/java/com/andreirookie/ots_basic_activities/ActivityB.kt
@@ -1,5 +1,6 @@
 package com.andreirookie.ots_basic_activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -8,14 +9,12 @@ class ActivityB : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
 
         val button = findViewById<Button>(R.id.toOpenActivityC)
-
         button.setOnClickListener {
-
+            val intent = Intent(this, ActivityC::class.java)
+            startActivity(intent)
         }
-
-
-        setContentView(R.layout.activity_b)
     }
 }
--- app/src/main/java/com/andreirookie/ots_basic_activities/ActivityC.kt
+++ app/src/main/java/com/andreirookie/ots_basic_activities/ActivityC.kt
@@ -1,5 +1,7 @@
 package com.andreirookie.ots_basic_activities
 
+import android.content.Intent
+import android.content.Intent.*
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -8,28 +10,32 @@ class ActivityC : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c)
 
         val buttonToOpenActivityA =findViewById<Button>(R.id.toOpenActivityA)
         val buttonToOpenActivityD =findViewById<Button>(R.id.toOpenActivityD)
         val buttonToCloseActivityC =findViewById<Button>(R.id.toCloseActivityC)
         val buttonToCloseStack =findViewById<Button>(R.id.toCloseStack)
 
-
         buttonToOpenActivityA.setOnClickListener {
-
+            val intent = Intent(this, ActivityA::class.java)
+            startActivity(intent)
         }
 
         buttonToOpenActivityD.setOnClickListener {
-
+            val intent = Intent(this, ActivityD::class.java)
+            intent
+                .addFlags(FLAG_ACTIVITY_CLEAR_TASK)
+                .addFlags(FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
         }
 
         buttonToCloseActivityC.setOnClickListener {
-
+            this.finish()
         }
 
         buttonToCloseStack.setOnClickListener {
-
+            this.finishAffinity()
         }
-        setContentView(R.layout.activity_c)
     }
 }
